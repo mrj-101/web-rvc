ddd020c8c639c4c30d0e3500876ee111007e2a92
Surachai Visesvoharn <mrj@rvc.ac.th>
2026-01-05 14:10:07 +0700

    แก้ไข index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,5 @@
 <template>
     <div>
-        <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-            <UPageCard
-                v-for="item in news"
-                :description="item.detail"
-            >
-                <template #title>
-                    <img :src="item.image" />
-                    <h2>{{item.title}}</h2>
-                </template>
-            </UPageCard>
-        </div>
+        <h1>Hello</h1>
     </div>
 </template>
-
-<script setup>
-    let news = ref(null)
-
-    await $fetch('http://localhost:9000/api/news')
-        .then(result => {
-            if (result.success) {
-                news.value = result.rows
-            }
-        })
-</script>
